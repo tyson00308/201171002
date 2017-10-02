c8656aa6a69fcfee9039b209c08c6b717d25e9b4
unknown <a098708394@gmail.com>
2017-10-02 15:09:28 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String str;
+	str=in.nextLine();
+	System.out.println(str.toLowerCase());
+
+    }
+}
